596284c001fe5f70934c6a9994da2852de29bd64
Albert Papp <albert.papp@blacksalt.io>
2018-05-13 15:24:01 +0200

    Pimped up demo server

--- bin/server.dart
+++ bin/server.dart
@@ -2,6 +2,26 @@ import 'dart:io';
 import 'dart:async';
 import 'package:http_server/http_server.dart';
 
+void returnIndexFile(HttpRequest request, String msg, String content) {
+  request.response.headers..contentType = ContentType.HTML;
+  request.response
+    ..writeln('<!DOCTYPE html>')
+    ..writeln('<html>')
+    ..writeln('<head>')
+    ..writeln("<link rel='stylesheet' href='index.css'>")
+    ..writeln("</head>")
+    ..writeln("<body>")
+    ..writeln("<h1>Hello from Dart server</h1>")
+    ..writeln("<p><a href='/'>Home</a></p>")
+    ..writeln("<p><a href='/add'>Add messages</a></p>")
+    ..writeln("<p><a href='/delete'>Delete messages</a></p>")
+    ..writeln("<p><a href='/version'>Server version</a></p>")
+    ..writeln("$msg\n<hr>")
+    ..writeln("<div><p>$content</p></div>")
+    ..writeln("</body>")
+    ..writeln("</html>")
+    ..close();
+}
 
 void main() {
   var webFiles = new VirtualDirectory('web');
@@ -9,37 +29,45 @@ void main() {
   runZoned(() {
     HttpServer.bind('0.0.0.0', 8080).then((server) {
       server.listen((request) {
-          if (request.uri.path == '/') {
-            request.response.redirect(request.uri.resolve('/index.html'));
-          } else if (request.uri.path == '/version') {
-            request.response.headers..contentType = ContentType.TEXT;
-            request.response
-                ..writeln('Dart version: ${Platform.version}')
-                ..writeln('Dart executable: ${Platform.executable}')
-                ..writeln('Dart executable arguments: '
-                          '${Platform.executableArguments}')
-                ..close();
-          } else if (request.uri.path == '/add') {
-      try {           new File('data/file.txt',mode:  FileMode.APPEND ).writeAsString('Dart is awsome!<br/>')
-    .then((File file) {
-       request.response.headers..contentType = ContentType.TEXT;
-            request.response
-                ..writeln('Added msg to file');
-// Do something with the file.
-    }); } catch(e) { request.response.redirect(request.uri.resolve('/index.html'));}
-          } else if (request.uri.path == '/list') {
+        if (request.uri.path == '/') {
+          print("Try to load file");
+          try {
             new File('data/file.txt').readAsString().then((String contents) {
-            request.response
-                ..writeln(contents); });
+              returnIndexFile(
+                  request, 'File size=${contents.length}', contents);
+            });
+          } catch (e) {
+            print(e);
+            returnIndexFile(request, 'No messages found', '');
           }
-          else {
-            webFiles.serveRequest(request);
-          }
+        } else if (request.uri.path == '/version') {
+          request.response.headers..contentType = ContentType.TEXT;
+          request.response
+            ..writeln('Dart version: ${Platform.version}')
+            ..writeln('Dart executable: ${Platform.executable}')
+            ..writeln('Dart executable arguments: '
+                '${Platform.executableArguments}')
+            ..close();
+        } else if (request.uri.path == '/add') {
+          try {
+            new File('data/file.txt')
+                .writeAsString('Dart is awsome!<br/>\n', mode: FileMode.APPEND)
+                .then((File file) {
+              file.readAsString().then((String contents) {
+                returnIndexFile(
+                    request, 'File size=${contents.length}', contents);
+              });
+            });
+          } catch (e) {}
+        } else if (request.uri.path == '/delete') {
+          new File('data/file.txt').deleteSync();
+          returnIndexFile(request, 'File has been deleted', '');
+        } else {
+          webFiles.serveRequest(request);
+        }
       });
     });
-  },
-  onError: (e, stackTrace) {
+  }, onError: (e, stackTrace) {
     print('Error processing request $e\n$stackTrace');
   });
 }
-
